dd43a347ab865bb3ca03bbce235575f17346e48d
Ravinder Bhadhal <ravinderbhadhal@Ravinders-iMac.local>
2018-08-06 16:43:05 +0100

    simple login screen

--- would-you-rather/src/components/Dashboard.js
+++ would-you-rather/src/components/Dashboard.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Poll from './poll'
+import { setAuthedUser } from '../actions/authedUser'
 
 class Dashboard extends Component {
   state = {
-    display: 'unanswered'
+    display: 'unanswered',
+    loggedIn: '',
   }
 
   handleChange = (e) => {
     const v = e.target.value
     this.setState(() => ({
-      display: v
+      display: v,
+
     }))
   }
-
+  handleUserChange = (e) => {
+    const u = e.target.value
+    const dispatch = this.props.dispatch
+    dispatch(setAuthedUser(u))
+    this.setState(() => ({
+      loggedIn: u,
+    }))
+  }
+// TODO: sort the answered array by timestamp
   render(){
 
     const allPolls = this.props.pollIds
     const answered = this.props.answered
     const unanswered = allPolls.filter(f => !answered.includes(f))
 
+
     return(
+      !this.state.loggedIn ? (
+        <form>
+          <label>
+            Please login first:
+          <select value={this.state.loggedIn} onChange={this.handleUserChange}>
+            <option value='tylermcginnis'>tylermcginnis</option>
+            <option value='johndoe'>johndoe</option>
+            <option value='sarahedo'>sarahedo</option>
+          </select>
+        </label>
+        </form>
+
+      ) :
       <div>
         <h3 className='center'> Your Questions </h3>
         <form >
@@ -52,10 +77,11 @@ class Dashboard extends Component {
 
       </div>
     )
+
   }
 }
 
-function mapStateToProps({polls, authedUser, users}){
+function mapStateToProps({polls, authedUser, users}, props){
   console.log(`in map state dashboard and authed user is ${authedUser} and user is ${Object.keys(users[authedUser].answers)}`)
   return{
 
